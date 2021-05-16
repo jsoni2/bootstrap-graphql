6f20fb47c5116b8c009c64d0c239db2cfc4aac54
Janak Soni <Janak.X.Soni@kp.org>
2021-05-16 10:37:24 -0500

    Initial commit

---
+++ src/main/java/com/udacity/bootstrapgraphql/BootstrapGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.udacity.bootstrapgraphql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapGraphqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapGraphqlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/udacity/bootstrapgraphql/BootstrapGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udacity.bootstrapgraphql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapGraphqlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
